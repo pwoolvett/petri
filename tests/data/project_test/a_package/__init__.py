759949674592dfc535cbc20d59bc7417ff6a7052
Pablo Woolvett <pablowoolvett@gmail.com>
2019-07-15 14:49:11 -0400

    corrected basesettings inheritance & testing

--- tests/data/project_test/a_package/__init__.py
+++ tests/data/project_test/a_package/__init__.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
-"""sample python module using petri"""
+"""A package: sample minimal package using petri"""
 
-from petri import initialize
+from petri import BaseSettings, initialize, LogMode, LogLevel
+
+
+class ProdSettings(BaseSettings):
+    ENV = "production"
+    LOG_LEVEL = LogLevel.TRACE
+    LOG_MODE = LogMode.ERROR_FILE
+
+
+class DevSettings(BaseSettings):
+    ENV = "development"
+    LOG_LEVEL = LogLevel.INFO
+    LOG_MODE = LogMode.CONSOLE | LogMode.ERROR_FILE
+
+
+class TestSettings(BaseSettings):
+    ENV = "testing"
+    LOG_LEVEL = LogLevel.ERROR
+    LOG_MODE = LogMode.CONSOLE
+
 
 __meta__, DOTENV_LOCATION, SETTINGS, LOGGER, _ = initialize(
     __file__, "a_package"
--- tests/data/script_test/script_example.py
+++ tests/data/script_test/script_example.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
 """sample python module using petri"""
 
-from petri import initialize
+from petri import BaseSettings, initialize, LogMode, LogLevel
+
+
+class ProdSettings(BaseSettings):
+    ENV = "production"
+    LOG_LEVEL = 40
+    LOG_MODE = LogMode.ERROR_FILE
+
+
+class DevSettings(BaseSettings):
+    ENV = "development"
+    LOG_LEVEL = LogLevel.INFO
+    LOG_MODE = LogMode.CONSOLE
+
+
+class TestSettings(BaseSettings):
+    ENV = "testing"
+    LOG_LEVEL = LogLevel.ERROR
+    LOG_MODE = LogMode.CONSOLE
+
 
 __meta__, DOTENV_LOCATION, SETTINGS, LOGGER, _ = initialize(
     __file__, "script_example"
