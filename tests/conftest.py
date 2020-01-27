3f3dd29ba32ba99674c0ac64442a9dfe07d08ae2
Pablo Woolvett <pablo.wooveltt@iconstruye.onmicrosoft.com>
2020-01-27 15:50:37 -0300

    ADD,TEST: added petri and pyproject toml fixtures
    Signed-off-by: Pablo Woolvett <pablo.wooveltt@iconstruye.onmicrosoft.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,28 @@
+import importlib
+import sys
 from pathlib import Path
 
 import pytest
 import toml
 
-@pytest.fixture(name='pyproject_toml')
-def pyproject_toml_fixture():
+
+@pytest.yield_fixture(name="imported_petri")
+def import_petri_fixture():
+    if "petri" in globals():
+        assert sys.getrefcount(a_pkg) == 3
+        del petri
+
+    if "petri" in sys.modules:
+        del sys.modules["petri"]
+
     import petri
+
+    importlib.reload(petri)
+    return petri
+
+
+@pytest.fixture(name="pyproject_toml")
+def pyproject_toml_fixture():
     pyproject_file = Path(__file__).parent.parent / "pyproject.toml"
     poetry = toml.load(pyproject_file)["tool"]["poetry"]
     return {str(k): v for k, v in poetry.items()}
