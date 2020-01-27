30da7221fa5a5ad9362a2bcd57d9dc47c33be190
Pablo Woolvett <pablo.wooveltt@iconstruye.onmicrosoft.com>
2020-01-27 15:50:55 -0300

    TEST,ADD: full test_init
    Signed-off-by: Pablo Woolvett <pablo.wooveltt@iconstruye.onmicrosoft.com>

---
+++ tests/unit/test_init.py
@@ -0,0 +1,14 @@
+def test_import(imported_petri):
+    assert imported_petri
+
+
+def test_bootstrap(imported_petri, pyproject_toml):
+    petri = imported_petri
+    pkg = petri.pkg
+    assert pkg
+
+    expected = {"name": "petri", "version": petri.__version__}
+    for name, value in expected.items():
+        assert getattr(pkg.meta, name) == value
+
+    petri.__version__ == pyproject_toml["version"]
